14ccc7c882a71b720c18b0fb4df47ec1c62fae85
Omninimbus006 <omninimbus006@gmail.com>
2026-07-22 18:06:54 -0400

    Finished status system

--- Assets/Scripts/StatusManager.cs
+++ Assets/Scripts/StatusManager.cs
@@ -1,16 +1,50 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StatusManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public readonly List<Status> Statuses = new List<Status>();
+    
+    private void Update()
     {
-        
+        for (int i = 0; i < Statuses.Count; i++)
+        {
+            Status status = null;
+            try
+            {
+                status = Statuses[i];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                break;
+            }
+            
+            status.Tick(Time.deltaTime);
+            if (status.Expired)
+            {
+                Statuses.Remove(status);
+                i--;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Adds a status to the object.
+    /// </summary>
+    /// <param name="status">The status to add.</param>
+    public void AddStatus(Status status)
     {
-        
+        Statuses.Add(status);
+    }
+
+    /// <summary>
+    /// Tries to remove a status from this object.
+    /// </summary>
+    /// <param name="status">The status to add.</param>
+    /// <returns>True if the status was removed. False if the status was not found.</returns>
+    public bool TryRemoveStatus(Status status)
+    {
+        return Statuses.Remove(status);
     }
 }
